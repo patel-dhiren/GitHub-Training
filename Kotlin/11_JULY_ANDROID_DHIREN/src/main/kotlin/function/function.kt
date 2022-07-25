dbb514daa2c2099c40f759b1e3ab428978fe6a28
Dhiren-Patel <dhirenpatel.tops@gmail.com>
2022-07-25 08:50:26 +0530

    factorial function created

--- Kotlin/11_JULY_ANDROID_DHIREN/src/main/kotlin/function/function.kt
+++ Kotlin/11_JULY_ANDROID_DHIREN/src/main/kotlin/function/function.kt
@@ -2,4 +2,18 @@ package function
 
 fun main() {
 
+    factorial()
+
+}
+
+fun factorial(){
+    var num = 5
+    var fact = 1
+
+    for(i in 1..num){
+        fact*=i
+    }
+
+    println("factorial : $fact")
+
 }
